5d68b85f019d6f0c11dfe7479046b0157443d0c7
Antonio Vargas <iam@antoniovargas.io>
2019-01-29 19:07:08 -0600

    Initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Indecision App"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some info"
+  )
+);
+
+ReactDOM.render(template, document.querySelector("#app"));
+"use strict";
---
+++ src/App.js
@@ -0,0 +1,8 @@
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+  </div>
+);
+
+ReactDOM.render(template, document.querySelector("#app"));
